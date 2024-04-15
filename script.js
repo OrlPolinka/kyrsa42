4d65fd6dc5216e3ffe8ddc45bded6e794b24daf3
Polina <porlovslaya7@gmail.com>
2024-04-16 01:01:53 +0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    let arrayParg = [];
 
-    fetch('js/index.xml')
+    fetch('index.xml')
         .then(function (response) {
             return response.text();
         })
         .then(function (xmlDocText) {
-            let arrayParg = [];
             const parser = new DOMParser();
             const xmlDoc = parser.parseFromString(xmlDocText, "text/xml");
             const MenuCards = xmlDoc.querySelectorAll('container1');
@@ -16,10 +16,10 @@ window.addEventListener('DOMContentLoaded', () => {
                 const imgScr = student.querySelector("imagine__src").textContent;
                 const linkScr = student.querySelector("link__href").textContent;
                 const title = student.querySelector("title").textContent;
-                const par1 = student.querySelector('par1');
-                const par2 = student.querySelector('par2');
-                const par3 = student.querySelector('par3');
-                arrayParg.push(par1, par2, par3);
+                const par1 = student.querySelector('par1').textContent;
+                const par2 = student.querySelector('par2').textContent;
+                const par3 = student.querySelector('par3').textContent;
+                arrayParg.push({par1, par2, par3});
                 tableBody.innerHTML += `
                 <div class="container1">
                 <h2>${title}</h2>
@@ -27,13 +27,13 @@ window.addEventListener('DOMContentLoaded', () => {
                     <a href="${linkScr}"><img src="${imgScr}" width="500px" height="300px">
                     <span class="s1">Подробнее...</span></a>
                     <div class="blok1">
-                        <p>${arrayParg[0]}</p>
-                        <p>${arrayParg[1]}</p>
-                        <p>${arrayParg[2]}</p>
+                        <p>${arrayParg[0].par1}</p>
+                        <p>${arrayParg[0].par2}</p>
+                        <p>${arrayParg[0].par3}</p>
                     </div>
                 </div>
             </div>
-            `
+            `;
             });
-        })
-    }
+        });
+});
