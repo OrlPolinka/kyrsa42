02fb0e3823924158a20c381f10f3a15e6dc51d59
OrlPolinka <porlovskaya7@gmail.com>
2024-05-17 00:59:50 +0300

    .

--- js/script1.js
+++ js/script1.js
@@ -3,39 +3,39 @@ window.addEventListener("DOMContentLoaded", function() {
     const roulette = document.querySelector('.roulette .images');
     const prevBtn = document.querySelector('.prev-btn');
     const nextBtn = document.querySelector('.next-btn');
-    const numImages = roulette.children.length; // Общее количество изображений
-    const imageWidth = roulette.firstElementChild.clientWidth; // Ширина каждого изображения
+    const numImages = roulette.children.length;
+    const imageWidth = roulette.firstElementChild.clientWidth;
     let currentIndex = 0;
-    let intervalId; // Идентификатор интервала для автоматической прокрутки
+    let intervalId;
 
     // Функция для обновления ширины изображения и перерисовки рулетки
     function updateRouletteWidth() {
-        const imageWidth = roulette.firstElementChild.clientWidth; // Получаем ширину каждого изображения
+        const imageWidth = roulette.firstElementChild.clientWidth;
         roulette.style.transition = 'none'; // Отключаем переход для предотвращения анимации при изменении размеров
         roulette.style.transform = `translateX(-${currentIndex * imageWidth}px)`; // Перемещаем рулетку на текущий индекс
     }
 
-    window.addEventListener('resize', updateRouletteWidth); // Добавляем обработчик события изменения размера окна
+    window.addEventListener('resize', updateRouletteWidth);
     
     prevBtn.addEventListener('click', function() {
-        currentIndex = (currentIndex - 1 + numImages) % numImages; // Обновляем индекс с учетом зацикливания
+        currentIndex = (currentIndex - 1 + numImages) % numImages;
         updateRoulette();
     });
 
     nextBtn.addEventListener('click', function() {
-        currentIndex = (currentIndex + 1) % numImages; // Обновляем индекс с учетом зацикливания
+        currentIndex = (currentIndex + 1) % numImages; 
         updateRoulette();
     });
 
     function updateRoulette() {
-        roulette.style.transition = 'transform 0.5s ease'; // Устанавливаем переход для плавного перемещения
+        roulette.style.transition = 'transform 0.5s ease'; 
         roulette.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
     }
 
     // Функция для автоматической прокрутки каждые 3 секунды
     function startAutoScroll() {
         intervalId = setInterval(function() {
-            currentIndex = (currentIndex + 1) % numImages; // Прокручиваем к следующему изображению
+            currentIndex = (currentIndex + 1) % numImages; 
             updateRoulette();
         }, 3000); // Каждые 3 секунды
     }
